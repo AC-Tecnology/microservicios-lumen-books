661fff2904b59be13f6d867198eb201315c365ae
gnuxdar <gnuxdar@gmail.com>
2021-03-10 16:17:31 -0500

    seeder to data example

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * la manera mas simple de poblar la tabla que del DatabaseSeeder.php de Authors
+ */
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +20,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
+        //factory(Book::class, 30)->create();
+        Book::factory()->count(30)->create();
     }
 }
